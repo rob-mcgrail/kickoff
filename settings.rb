0477bf4be0b8db8eb5111e61bb0e0f3a37b438d1
Rob McGrail <rob@aichi.(none)>
2012-02-12 01:33:16 +1300

    tidyup

--- settings.rb
+++ settings.rb
@@ -2,7 +2,7 @@ configure do
   set :method_override, true # For HTTP verbs
   set :sessions, true
   set :logging, false # stops annoying double log messages.
-  set :static, false # see config.ru for dev mode satatic file serving
+  set :static, false # see config.ru for dev mode static file serving
   set :redis, 1 # redis database
 end
 
